84bb7f74a4f903f6213700854ec0dc25c437a4ba
jack <462503082@qq.com>
2020-12-10 15:30:30 +0800

    再次修改

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ public class Main {
     @Test
     public void testHelloWrold() {
 
+        System.out.println("goood!");
         System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
+        System.out.println("HeWorld!");
         System.out.println("HelloWorld!");
     }
 }
